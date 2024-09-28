6cc7c89dade04f71039dd2e4231a43cfd7cea401
Isabel <isabelleewy@gmail.com>
2024-09-28 02:19:17 +0200

    Update ocrController.js

--- backend/controllers/ocrController.js
+++ backend/controllers/ocrController.js
@@ -4,31 +4,21 @@ const fs = require("fs");
 
 exports.addExpense = async (req, res) => {
   try {
-    // Check if the file is provided
     if (!req.file) {
       return res.status(400).json({ error: "No file uploaded" });
     }
 
     const imagePath = req.file.path;
-    // Use Tesseract to extract text
     const { data: { text } } = await Tesseract.recognize(
         imagePath,
         'eng'
       );
 
-    console.log("Extracted Text:", text);
     const userId = req.body.userId;
 
-    const batch = admin.firestore().batch();
-    expenses.forEach(expense => {
-      const expenseRef = admin.firestore().collection("users").doc(userId).collection("expenses").doc();
-      batch.set(expenseRef, {
-        description: expense.description,
-        category: expense.category,
-        name: expense.name,
-        amount: expense.amount,
+    await admin.firestore().collection("receipts").add({
+        text: text,
         createdAt: new Date(),
-      });
     });
 
     res.status(200).json({
