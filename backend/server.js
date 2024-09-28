a15f2c376bc63dcf2e5489e7ef84ac6cebdb5865
Isabel <isabelleewy@gmail.com>
2024-09-28 06:57:59 +0200

    Remove unused code in server.js

--- backend/server.js
+++ backend/server.js
@@ -36,27 +36,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-
-const db = admin.firestore();
-
-// Example endpoint to test adding user data
-app.post('/addUser', async (req, res) => {
-    try {
-        console.log(req.body); 
-        const { name, email } = req.body; // Get data from request body
-        const userRef = await db.collection('users').add({
-            name: name, // Use the name from the request
-            email: email // Use the email from the request
-        });
-        res.status(201).send(`User added with ID: ${userRef.id}`);
-    } catch (error) {
-        console.error('Error adding user:', error);
-        res.status(500).send('Error adding user.');
-    }
-});
-
-
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
