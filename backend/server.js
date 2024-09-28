89e4a6c220ee215e54d87ca1dc327c95caa4e7fc
Isabel <isabelleewy@gmail.com>
2024-09-28 08:06:09 +0200

    Add endpoint to addUser

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-console.log('OpenAI API Key:', process.env.OPENAI_API_KEY);
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
@@ -15,7 +15,7 @@ const ocrRouter = require('./routes/ocrRoutes');
 
 
 const app = express();
-
+app.use(cors());
 
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -31,12 +31,30 @@ app.use(bodyParser.json());
 app.use('/', llmRouter);
 app.use('/', ocrRouter);
 
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+    res.send('API is running'); // Simple text response
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+const db = admin.firestore();
+
+// Example endpoint to test adding user data
+app.post('/addUser', async (req, res) => {
+    try {
+        console.log(req.body); 
+        const { name, email } = req.body; // Get data from request body
+        const userRef = await db.collection('users').add({
+            name: name, // Use the name from the request
+            email: email // Use the email from the request
+        });
+        res.status(201).send(`User added with ID: ${userRef.id}`);
+    } catch (error) {
+        console.error('Error adding user:', error);
+        res.status(500).send('Error adding user.');
+    }
+});
+
+
+const ipAddress = '192.168.0.121'; // Replace with your actual IP address
+app.listen(port, ipAddress, () => {
+    console.log(`Server is running at http://${ipAddress}:${port}`);
 });
